5fcc38d9c5d6cf770a8e5cb0d90ffe9ef91743c2
PouryaHOZ <asnew.d.06@gmail.com>
2026-02-14 01:49:38 +0330

    - Added "try" and "exception" - Seperated "filter" and "save" parts from each other

--- main.py
+++ main.py
@@ -26,46 +26,55 @@ def fetch_API(query:str):
         "q":query
     }
 
-    respose = requests.get(API_URL, params=params)
-    data = respose.json()
-    docs = data.get("docs", [])
-    return docs
+    try:
+        respose = requests.get(API_URL, params=params)
+        data = respose.json()
+        books = data.get("docs", [])
+        return books
+    except:
+        raise ValueError("The API couldn't fetch with Openlibrary")
 
-# Choosing fields to save
-def filter_and_save(docs):
+
+# Filtering all the books to after year 2000
+def filtering(books):
+    try:
+        return filter(lambda x: x["first_publish_year"] > 2000, books)
+    except:
+        raise ValueError("There was an error while filtering the books (docs)")
+
+# Saving books to the books.csv file
+def save(books_filtered):
+
+    # Choosing fields to save, and starting the counter
     fields = ["title", "author_name", "first_publish_year", "publisher"]
-
-    # Filtering and saving books to the CSV file
-
     saved_books_num = 0
 
-    with open("books.csv", "w", newline="", encoding="utf-8") as f:
-        writer = csv.DictWriter(f, fieldnames=fields)
-        writer.writeheader()
+    # Starting the process, using CSV writer
+    try:
+        with open("books.csv", "w", newline="", encoding="utf-8") as f:
+            writer = csv.DictWriter(f, fieldnames=fields)
+            writer.writeheader()
 
-        for doc in docs:
-            first_publish_year = doc.get("first_publish_year")
-        
-            if (first_publish_year > 2000):
-                row = {
-                    "title": doc.get("title"),
-                    "author_name": doc.get("author_name"),
-                    "first_publish_year": doc.get("first_publish_year"),
-                    "publisher": doc.get("publisher"),
-                }
-                writer.writerow(row)
+            for book in books_filtered:
+                    row = {
+                        "title": book.get("title"),
+                        "author_name": book.get("author_name"),
+                        "first_publish_year": book.get("first_publish_year"),
+                        "publisher": book.get("publisher"),
+                    }
+                    writer.writerow(row)
 
-                saved_books_num += 1
+                    saved_books_num += 1
     
-    return saved_books_num
-
-# Sending "Success Message"
-
-
+        return saved_books_num
+    except:
+        raise ValueError("There was an error saving filtered books (docs) into books.csv")
 
 if __name__ == "__main__":
     q = get_query()
-    docs = fetch_API(q)
-    saved_num = filter_and_save(docs)
+    books = fetch_API(q)
+    filtered_books = filtering(books)
+    saved_num = save(filtered_books)
 
+    # Sending "Success Message"
     print("Saved", saved_num, "books in books.csv")
